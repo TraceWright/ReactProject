f7a351917d204d83b9a67a91c01e581c220e9c1d
Tracey Wright <traceywright27@gmail.com>
2017-08-19 20:46:57 +1000

    interim commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,163 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import express from 'express';
+// import { saveAs } from 'file-saver';
+import injectTapEventPlugin  from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
 import './App.css';
+// import ChannelsListWithData from './components/ChannelsListWithData';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link 
+} from 'react-router-dom'
 
-import ApolloClient from 'apollo-client';
-import { graphql, ApolloProvider } from 'react-apollo';
-import gql from 'graphql-tag';
-
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
-import { typeDefs } from './schema';
-
-const schema = makeExecutableSchema({ typeDefs });
-addMockFunctionsToSchema({ schema });
-
-const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
+// import {
+//   ApolloClient,
+//   ApolloProvider,
+// } from 'react-apollo'; 
 
 
-const client = new ApolloClient({
-  networkInterface: mockNetworkInterface,
-});
+// import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+// import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
+// import { typeDefs } from './schema';
 
-const ChannelsList = ({ data: {loading, error, channels }}) => {
-  if (loading) {
-    return <p>Loading ...</p>;
-  }
-  if (error) {
-    return <p>{error.message}</p>;
-  }
+injectTapEventPlugin();
 
-  return <ul>
-    { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
-  </ul>;
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const Login = () => (
+  <div>
+    <h2>Login</h2>
+  </div>
+)
+
+// app.post('/login', passport.authenticate('local', { successRedirect: '/',
+// failureRedirect: '/login' }));
+
+export const routes = () => (
+  <Router>
+    <div>
+      <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/login">Login</Link></li>
+      </ul>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/login" component={Login}/>
+    </div>
+  </Router>
+); 
+
+// const schema = makeExecutableSchema({ typeDefs });
+// addMockFunctionsToSchema({ schema });
+
+// const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
+ 
+// const client = new ApolloClient({
+//   networkInterface: mockNetworkInterface,
+// });
+
+function personRequest() {
+  const params = {
+      method: 'GET',
+      keepalive: true,
+      headers: {
+          Accept: 'application/json'
+      },
+  };
+  const result = fetch('http://localhost:4000/person/', params);
+  result.then(response => {
+    return response.json();
+  }).then(data => {
+    console.log(data);
+    return data;
+  })
 };
 
-const channelsListQuery = gql`
-  query ChannelsListQuery {
-    channels {
-      id
-      name
-    }
-  }
-`;
+function showPerson() {
+  let data = personRequest();
+  console.log(data);
+}
 
-const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
+const style = {
+  display: 'inline-block',
+  margin: '16px 32px 16px 0',
+};
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
   render() {
-    return (
-      <ApolloProvider client={client}>
+    return ( 
+        
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to Apollo</h2>
-          </div>
-          <ChannelsListWithData />
+          
+          
+
+       <MuiThemeProvider>
+          <AppBar
+            title="My Awesome App"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          >
+           
+        
+       <Drawer open={this.state.open}>
+        <Menu desktop={true} width={320} style={style}>
+        <MenuItem primaryText="Single" insetChildren={true} />
+        <MenuItem primaryText="1.15" insetChildren={true} />
+        <MenuItem primaryText="Double" insetChildren={true} />
+        <MenuItem
+          primaryText="Custom: 1.2"
+          checked={true}
+          rightIcon={<ArrowDropRight />}
+          menuItems={[
+            <MenuItem
+              primaryText="Show"
+              rightIcon={<ArrowDropRight />}
+              menuItems={[
+                <MenuItem primaryText="Show Level 2" />,
+                <MenuItem primaryText="Grid lines" checked={true} />,
+                <MenuItem primaryText="Page breaks" insetChildren={true} />,
+                <MenuItem primaryText="Rules" checked={true} />,
+              ]}
+            />,
+            <MenuItem primaryText="Grid lines" checked={true} />,
+            <MenuItem primaryText="Page breaks" insetChildren={true} />,
+            <MenuItem primaryText="Rules" checked={true} />,
+          ]}
+        />
+        <Divider />
+        <MenuItem primaryText="Add space before paragraph" />
+        <MenuItem primaryText="Add space after paragraph" />
+        <Divider />
+        <MenuItem primaryText="Custom spacing..." />
+        </Menu>
+          </Drawer>
+          </AppBar>
+        </MuiThemeProvider>
+
+        <Home/>
+          <button onClick={showPerson}>List People</button>
+
         </div>
-      </ApolloProvider>
     );
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,69 @@
 import express from 'express';
-
-const PORT = 4000;
+// import {
+//   graphqlExpress,
+//   graphiqlExpress,
+// } from 'graphql-server-express';
+//import bodyParser from 'body-parser';
+import cors from 'cors';
+//import { schema } from './src/schema';
+//import { postgraphql, createPostGraphQLSchema, withPostGraphQLContext } from 'postgraphql';
+import { Pool } from 'pg';
 
 const server = express();
+const PORT = 4000;
 
-server.get('/', function (req, res) {
-  res.send('Hello World!');
+const pgPool = new Pool({
+  database: 'adventureworks',
+  user: 'tracey',
+  password: 'password',
+  host: 'localhost',
+  port: 5432, 
+  postgraphql: {
+    schema: 'person',
+    
+  }
 });
 
-server.listen(PORT, () => console.log(`GraphQL Server is now running on http://localhost:${PORT}`));
+server.use('*', cors({ origin: 'http://localhost:3000' }));
+
+server.get('/person', function(req, res){
+  pgPool.query('Select * from person.person limit 100').then(function(data){
+    console.log(data);
+    res.send(data);
+  }, function(err){res.send(500,{error: err})})
+});
+
+
+
+
+// pgPool.query('Select * from person.person').then(people => {
+//   console.log(people.rows[0]);
+// })
+
+ 
+
+
+
+// server.use('/graphql', bodyParser.json(), graphqlExpress({
+//   schema
+// }));
+
+// server.use('/graphiql', graphiqlExpress({
+//   endpointURL: '/graphql' 
+// }));
+
+// server.use(postgraphql('postgres://tracey:password@localhost:5432/adventureworks'));
+
+// server.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+//server.listen(3000)
+
+//const pgsql = postgraphql(pgpool);
+// server.use(pgsql);
+//server.listen(3000);
+ 
+server.listen(PORT, () => 
+  console.log(`GraphQL Server is now running on http://localhost:${PORT}/graphiql`)
+);
