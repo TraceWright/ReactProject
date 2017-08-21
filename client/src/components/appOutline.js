700173d2633338814e073ecff3c70550e2e4fdeb
Tracey Wright <traceywright27@gmail.com>
2017-08-21 12:08:24 +1000

    interim commit

--- client/src/components/appOutline.js
+++ client/src/components/appOutline.js
@@ -14,21 +14,8 @@ import { People } from './people';
 
 injectTapEventPlugin();
 
-// export const routes = () => (
-
-// );
-
-const menuStyle = {
-  display: 'inline-block',
-  margin: '16px 32px 16px 0',
-  paddingTop: '50px',
-  
-};
-
-const menuItemStyle = {
-  textAlign: 'left', 
-  paddingLeft: '20px',
-}
+const menuStyle = { display: 'inline-block', margin: '16px 32px 16px 0', paddingTop: '50px' };
+const menuItemStyle = { textAlign: 'left', paddingLeft: '20px' }
 
 class AppOutline extends Component {
 
@@ -40,66 +27,61 @@ class AppOutline extends Component {
   handleToggle = () => this.setState({open: !this.state.open});
   handleClose = () => this.setState({open: false});
 
-    render() {
-        return (
+  render() {
+    return (
+      <div>    
+        <MuiThemeProvider>
+          <AppBar style={{zIndex: '1301', backgroundImage: 'linear-gradient(20deg, #2b3658 0%, #523e5b 100%)'}}
+            title="My Awesome App"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={this.handleToggle}
+          ></AppBar>
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <Router>
             <div>
-              
-            <MuiThemeProvider>
-            <AppBar style={{zIndex: '1301', backgroundImage: 'linear-gradient(20deg, #2b3658 0%, #523e5b 100%)'}}
-              title="My Awesome App"
-              iconClassNameRight="muidocs-icon-navigation-expand-more"
-              onLeftIconButtonTouchTap={this.handleToggle}
-            ></AppBar>
-            </MuiThemeProvider>
-  
-            <MuiThemeProvider>
-            <Router>
-    <div>
-  
-         <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
-          <Menu desktop={true} style={menuStyle} autoWidth={true} >
-          <MenuItem primaryText="Home" style={menuItemStyle} containerElement={<Link to="/home"></Link>} />
-          <MenuItem primaryText="CSS Grid" style={menuItemStyle} containerElement={<Link to="/cssGrid"></Link>} />
-          <MenuItem primaryText="People" style={menuItemStyle} containerElement={<Link to="/people"></Link>} />
-          <MenuItem
-            primaryText="Menu Item"
-            style={menuItemStyle}
-            rightIcon={<ArrowDropRight />}
-            menuItems={[
-              <MenuItem
-                primaryText="Menu Item"
-                style={menuItemStyle}
-                rightIcon={<ArrowDropRight />}
-                menuItems={[
-                  <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
-                  <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
-                  <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
-                  <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
-                ]}
-              />,
-              <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
-              <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
-              <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
-            ]}
-          />
-          <Divider />
-          <MenuItem primaryText="Menu Item" style={menuItemStyle} />
-          <MenuItem primaryText="Menu Item" style={menuItemStyle} />
-          <Divider />
-          <MenuItem primaryText="Menu Item" style={menuItemStyle} />
-          </Menu>
-          </Drawer> 
-
-      {/* <Route exact path="/" component={Home}/> */}
-      <Route path="/home" component={Home}/>
-      <Route path="/cssGrid" component={CSSGrid}/>
-      <Route path="/people" component={People}/>
-    </div>
-  </Router>
-          </MuiThemeProvider>
-          
-          </div>
-        )}
+              <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+                <Menu desktop={true} style={menuStyle} autoWidth={true} >
+                  <MenuItem primaryText="Home" style={menuItemStyle} containerElement={<Link to="/home"></Link>} />
+                  <MenuItem primaryText="CSS Grid" style={menuItemStyle} containerElement={<Link to="/cssGrid"></Link>} />
+                  <MenuItem primaryText="People" style={menuItemStyle} containerElement={<Link to="/people"></Link>} />
+                  <MenuItem
+                    primaryText="Menu Item"
+                    style={menuItemStyle}
+                    rightIcon={<ArrowDropRight />}
+                    menuItems={[
+                      <MenuItem
+                        primaryText="Menu Item"
+                        style={menuItemStyle}
+                        rightIcon={<ArrowDropRight />}
+                        menuItems={[
+                          <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
+                          <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
+                          <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
+                          <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
+                        ]}
+                      />,
+                      <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
+                      <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
+                      <MenuItem primaryText="Menu Item" style={menuItemStyle} />,
+                    ]}
+                  />
+                  <Divider />
+                  <MenuItem primaryText="Menu Item" style={menuItemStyle} />
+                  <MenuItem primaryText="Menu Item" style={menuItemStyle} />
+                  <Divider />
+                  <MenuItem primaryText="Menu Item" style={menuItemStyle} />
+                </Menu>
+              </Drawer>             {/* <Route exact path="/" component={Home}/> */}
+            <Route path="/home" component={Home}/>
+            <Route path="/cssGrid" component={CSSGrid}/>
+            <Route path="/people" component={People}/>
+            </div>
+          </Router>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default AppOutline;
