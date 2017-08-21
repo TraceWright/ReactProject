f33b9618a6cc16d10412a8ff5cf5c2396c295132
Tracey Wright <traceywright27@gmail.com>
2017-08-21 13:28:39 +1000

    interim commit

--- client/src/components/cssGrid.js
+++ client/src/components/cssGrid.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../App.css';
+
+//https://gridbyexample.com/examples/
 
 export const CSSGrid = () => (
-    <div>
-      <h2>Griddy grid</h2>
+    <div className="appGrid">
+        <div className="box header"></div>
+        <div className="box leftside"></div>
+        <div className="box middle"></div>
+        <div className="box rightside"></div>
+        <div className="box footer"></div>
     </div>
   )
